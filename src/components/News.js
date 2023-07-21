9bff7416146249d924f4da4130b0b47187cf7d8f
Avinash Yadav <109527105+logan-14@users.noreply.github.com>
2023-07-22 00:45:53 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, {useEffect , useState}  from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
@@ -6,84 +6,80 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 const News = (props) => {
 
-   const [articles , setArticle] = useState([])
-   const [loading, setLoading] = useState(true)
-   const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
 
+  const [articles, setArticle] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
 
- const updateNews = async () => {
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const updateNews = async () => {
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=0edb215a385a4a51be19a1be8786bbb9&page=${page}&pageSize=${props.pageSize}`
-    setLoading(true );
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json()
-    
+
     setArticle(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
 
   }
- 
-  useEffect(()=>{
-    updateNews(); 
-  },[])
 
-  
- const  handleNext = async () => {
-    setPage(page + 1 );
+  useEffect(() => {
+    updateNews();
+  }, [])
+
+
+  const handleNext = async () => {
+    setPage(page + 1);
     updateNews();
   }
 
- const  handlePrev = async () => {
-    setPage(page - 1 );
+  const handlePrev = async () => {
+    setPage(page - 1);
     updateNews();
   }
-   
- const fetchMoreData =  async () =>{
-   
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=0edb215a385a4a51be19a1be8786bbb9&page=${page+1}&pageSize=${props.pageSize}`;
-    setPage(page+1)
+
+  const fetchMoreData = async () => {
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=0edb215a385a4a51be19a1be8786bbb9&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1)
     let data = await fetch(url);
     let parsedData = await data.json();
     setArticle(articles.concat(parsedData.articles));
-      setTotalResults(parsedData.totalResults);
-      setLoading(false);
-    }
-
-
-
-    return (
-      <div className='contaniner'>
-        <h2 className='text-center'>Top Headlines</h2>
-        {/* {state.loading && <Spinner />} */}
-        
-        <InfiniteScroll
-           dataLength={articles.length}
-           next={fetchMoreData}
-           hasMore={articles.length !==totalResults}
-           loader={<Spinner/>}
-        
-        >
-
-             <div className="container">
-
-             
-          <div className="row">
-          {articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-            </div>
-
-          })}
-        </div>
-        </div>
-        </InfiniteScroll>
-
-        
-       
-      </div>
-    )
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   }
+
+
+
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}> FEEDLY - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+
+          <div className="row">
+            {articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </>
+  )
+}
 
 
 News.defaultProps = {
